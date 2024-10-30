8c916743aeb3684fa56a180b402545209d20ec06
BellaJoThorn <bella.thorn@ada.ac.uk>
2024-10-30 12:26:16 +0000

    Final Commit - Clean

--- dfp.js
+++ dfp.js
@@ -8,31 +8,31 @@ function parseFile (indata, outdata) {
   }
   
   //Deletes the current output file 
-  fs.existsSync(outdata) ? fs.unlinkSync(outdata) : false
+  fs.existsSync(outdata) ? fs.unlinkSync(outdata) : false;
   
   //Reads the input file in read only mode and splits it into lines 
   const data = fs.readFileSync(indata, {encoding: "utf-8", flag: 'r'});
   const lines = data.split(/\n/);
   
   // Initialize a counter for the number of records processed
-  let numRecords = 0
+  let numRecords = 0;
   
   //Extracts the delimiter from the first line of the file
-  const delimiter = lines[0].at(6)
+  const delimiter = lines[0].at(6);
   
   //Processes each line to extract and format review data
   for (let i = 1; i < lines.length; i++) {
     const line = lines[i];
     const elements = line.split(delimiter);
-    const review = elements[0].trim().slice(0, 20)
-    const sentiment = elements[1].trim()
+    const review = elements[0].trim().slice(0, 20);
+    const sentiment = elements[1].trim();
 
     //Appends the formatted reviews and sentiments to the output file
     fs.appendFileSync(outdata, `${sentiment}${delimiter}${review}\n`, "utf-8");
-    numRecords++
+    numRecords++;
   }
 
-  return numRecords
+  return numRecords;
   
 }
 
